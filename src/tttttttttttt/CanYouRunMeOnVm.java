856fda23fabdd840d0e7387098d31dd1cab90a9f
drew_emig <drew_emig@CO01-JBKJNC2.mhf1.mhf.mhc>
2017-12-20 16:40:54 -0700

    l

--- src/tttttttttttt/CanYouRunMeOnVm.java
+++ src/tttttttttttt/CanYouRunMeOnVm.java
@@ -4,6 +4,7 @@ public class CanYouRunMeOnVm {
 
 	public static void main(String[] args) {
 		System.out.println("yay!");
+		System.out.println("really!");
 		// TODO Auto-generated method stub
 
 	}
